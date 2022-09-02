e9eb901564356d45549e3e5f03bc5c150f83aabf
DavidePisani <davide.pisani96@gmail.com>
2022-09-02 16:58:03 +0200

    first commit

---
+++ resources/js/views/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="container">
+        <a href="/admin"> Accedi</a>
+        <h1>Benvenuto nel nostro sito, fatto con VueJs </h1>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="container">
+        <h1> Benvenuto, 'utente'</h1>
+    </div>
+    
+@endsection
